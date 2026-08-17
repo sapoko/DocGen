e72d335c1ce8396f97f78ea27671776186a631da
Yan <yantuz2005@gmail.com>
2026-08-17 13:46:10 +0300

    Updated .yaml and cleaned up some code

--- src/main/java/Sapoko/docgen/document/DocumentRepository.java
+++ src/main/java/Sapoko/docgen/document/DocumentRepository.java
@@ -1,6 +1,5 @@
 package Sapoko.docgen.document;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
